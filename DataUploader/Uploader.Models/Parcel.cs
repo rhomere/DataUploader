55988f509448bd51a74e8aed4130b8734cbdbdfc
Renel Homere <rhomere@gmail.com>
2019-05-27 13:47:23 -0400

    revise Parcel class

--- DataUploader/Uploader.Models/Parcel.cs
+++ DataUploader/Uploader.Models/Parcel.cs
@@ -8,8 +8,22 @@ namespace Uploader.Models
 {
     public class Parcel
     {
-        public string Folio { get; set; }
-        public string Municiple { get; set; }
-        public string Description { get; set; }
+        public string MunicipalNumber { get; set; }
+        public string Owner { get; set; }
+        public string Owner2 { get; set; }
+        public string MailingAddressLine1 { get; set; }
+        public string MailingAddressLine2 { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Zip { get; set; }
+        public string SiteAddress { get; set; }
+        public string StreetNumber { get; set; }
+        public string StreetPrefix { get; set; }
+        public string StreetName { get; set; }
+        public string StreetNumberSuffix { get; set; }
+        public string StreetSuffix { get; set; }
+        public string CondoUnit { get; set; }
+        public string SiteCity { get; set; }
+        public string SiteZip { get; set; }
     }
 }
