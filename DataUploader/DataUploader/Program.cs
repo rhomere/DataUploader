d83e5e3f9d1f64f626c7fdc242be52a38a71df8a
Renel Homere <rhomere@gmail.com>
2019-05-27 12:42:29 -0400

    add Uplader library ref to DataUploader add uploaderSerivce use to Program.cs add loadWorkbook and processWorkSheet

--- DataUploader/DataUploader/Program.cs
+++ DataUploader/DataUploader/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Uploader;
 
 namespace DataUploader
 {
@@ -10,6 +11,9 @@ namespace DataUploader
     {
         static void Main(string[] args)
         {
+            var service = new UploaderService();
+            service.LoadWorkbook(@"C:\Users\rhomere\Downloads\sample.xlsx");
+            service.ProcessWorksheet(1);
         }
     }
 }
--- DataUploader/Uploader/UploaderService.cs
+++ DataUploader/Uploader/UploaderService.cs
@@ -3,10 +3,63 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Excel = Microsoft.Office.Interop.Excel;
 
 namespace Uploader
 {
     public class UploaderService
     {
+        Excel.Application XlApp = new Excel.Application();
+        Excel.Workbook XlWorkbook = null;// new Excel.Workbook();
+        Excel._Worksheet XlWorksheet = null;// new Excel.Worksheet();
+        Excel.Range XlRange = null;
+
+        public UploaderService()
+        {
+            
+        }
+
+        public void LoadWorkbook(string path)
+        {
+            try
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Loading...");
+                XlWorkbook = XlApp.Workbooks.Open(path);//@"sandbox_test.xlsx"
+                Console.WriteLine("Loading Complete.");
+            }
+            catch(Exception e)
+            {
+                Console.BackgroundColor = ConsoleColor.White;
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("\nError");
+                Console.WriteLine(e.Message);
+                Console.WriteLine("Stacktrace");
+                Console.WriteLine(e.StackTrace);
+                Console.ResetColor();
+            }
+        }
+
+        public void ProcessWorksheet(int sheetNum)
+        {
+            XlWorksheet = XlWorkbook.Sheets[sheetNum];
+            XlRange = XlWorksheet.UsedRange;
+
+            for (int i = 1; i <= XlRange.Rows.Count; i++)
+            {
+                for (int j = 1; j <= XlRange.Columns.Count; j++)
+                {
+                    //new line
+                    if (j == 1)
+                        Console.Write("\r\n");
+
+                    //write the value to the console
+                    if (XlRange.Cells[i, j] != null && XlRange.Cells[i, j].Value2 != null)
+                        Console.Write(XlRange.Cells[i, j].Value2.ToString() + "\t");
+
+                    //add useful things here!   
+                }
+            }
+        }
     }
 }
